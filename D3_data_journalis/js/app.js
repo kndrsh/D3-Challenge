f3d5399cff869e9598f97c36dee95366995a7fe7
Isha kundra <kndrsh@gmail.com>
2021-07-18 00:27:50 -0700

    final code for app

--- D3_data_journalis/js/app.js
+++ D3_data_journalis/js/app.js
@@ -39,4 +39,77 @@ d3.csv("assets/data/data.csv").then(function (stateData) {
     .domain([0, d3.max(stateData, d => d.healthcare)])
     .range([height, 0]);
 
- 
+  
+  // ==============================
+  var bottomAxis = d3.axisBottom(xLinearScale);
+  var leftAxis = d3.axisLeft(yLinearScale);
+
+  
+  // ==============================
+  chartGroup.append("g")
+    .attr("transform", `translate(0, ${height})`)
+    .call(bottomAxis);
+
+  chartGroup.append("g")
+    .call(leftAxis);
+
+  // 
+  // // ==============================
+  var circlesGroup = chartGroup.selectAll("circle")
+  .data(stateData)
+  .enter()
+  .append("circle")
+  .attr("cx", d => xLinearScale(d.poverty))
+  .attr("cy", d => yLinearScale(d.healthcare))
+
+  .attr("r", "12")
+  
+  .attr("fill", "blue")
+
+  .attr("opacity", ".5");  
+
+
+// ==============================
+var toolTip = d3.tip()
+  .attr("class", "tooltip")
+  .offset([80, -60])
+  .html(function (d) {
+    return (`${d.state}<br>Poverty: ${d.poverty}<br>Healthcare: ${d.healthcare}`);
+  });
+
+// 
+// ==============================
+chartGroup.call(toolTip);
+
+// 
+// ==============================
+circlesGroup.on("click", function (data) {
+  toolTip.show(data, this);
+})
+  // onmouseout event
+  .on("mouseout", function (data, index) {
+    toolTip.hide(data);
+  });
+
+// Create axes labels
+chartGroup.append("text")
+  .attr("transform", "rotate(-90)")
+  .attr("y", 0 - margin.left + 40)
+  .attr("x", 0 - (height / 2))
+  .attr("dy", "1em")
+  .attr("class", "axisText")
+  .style("fill", "black")
+  .style("font", "20px sans-serif")
+  .style("font-weight", "bold")
+  .text("People without Healthcare (%)");
+
+chartGroup.append("text")
+  .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+  .attr("class", "axisText")
+  .style("font", "20px sans-serif")
+  .style("font-weight", "bold")
+  .text("Poverty (%)");
+
+}).catch(function (error) {
+console.log(error);
+});
