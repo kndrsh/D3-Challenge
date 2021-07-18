4e5b98ce8b0e395364b2ec7b484b1764bb6cb269
Isha kundra <kndrsh@gmail.com>
2021-07-18 00:00:32 -0700

    x & Y scales

--- D3_data_journalis/js/app.js
+++ D3_data_journalis/js/app.js
@@ -12,3 +12,31 @@ var margin = {
 
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
+
+var svg = d3.select("#scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+d3.csv("assets/data/data.csv").then(function (stateData) {
+    console.log(stateData);
+    
+    // ==============================
+    stateData.forEach(function (data) {
+          data.poverty = +data.poverty;
+          data.healthcare = +data.healthcare;
+      });
+ 
+  // ==============================
+  var xLinearScale = d3.scaleLinear()
+    .domain([0, d3.max(stateData, d => d.poverty)])
+    .range([0, width]);
+
+  var yLinearScale = d3.scaleLinear()
+    .domain([0, d3.max(stateData, d => d.healthcare)])
+    .range([height, 0]);
+
+ 
